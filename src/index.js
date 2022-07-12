77b19dbd0c20686dc299e172a4ec793e93c1a64b
Koskei57 <dennis.koskei@student.moringaschool.com>
2022-07-12 17:59:04 +0300

    Done

--- src/index.js
+++ src/index.js
@@ -3,3 +3,10 @@ import ReactDOM from "react-dom";
 import App from "./App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
+
+// class Comment extends React.Component{
+// render(){
+// return <div>Naturally,I agree with this article.</div>
+// }
+// }
